8ac88e06c86a4ca9a4c00e1e21fa5815cc7425cc
cvb91 <tenacitylabs@protonmail.com>
2022-06-14 16:11:53 +0100

    updated products

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
       <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>The Winter Hammock Company</Navbar.Brand>
+            <Navbar.Brand className='logo'>The Winter Hammock Company</Navbar.Brand>
           </LinkContainer>
 
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
